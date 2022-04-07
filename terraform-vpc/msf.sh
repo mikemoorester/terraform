45d09d9dd90cbac1331d5b6b22882cf14e0ed182
Michael Moore <mikemoorester@gmail.com>
2022-04-07 01:21:07 +0100

    Add msf build scrips, need to change for ububuntu

--- terraform-vpc/msf.sh
+++ terraform-vpc/msf.sh
@@ -3,15 +3,33 @@
 # install metasploit onto kali instance
 
 # sleep until instance is ready
-until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
-  sleep 1
-done
+#until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+#  sleep 1
+#done
 
-# install nginx
-apt-get update
-#apt-get -y install nginx
+# update the repository info 
+#apt-get update
+apt install nmap
+# install form the nightly build of mfsconsole
 
 # make sure nginx is started
-sudo msfdb init
+#sudo msfdb init
 # Start the web service listening on any host address
-sudo msfdb --component webservice --address 0.0.0.0 start 
+#sudo msfdb --component webservice --address 0.0.0.0 start 
+
+#The following script invocation will import the Rapid7 signing key and setup the package for supported Linux and macOS systems:
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+
+/sbin/useradd -c cyber cyber -p cyber
+# Start the data base
+echo "Starting msf database"
+sudo -u cyber msfdb init 
+- include webservice at command line
+#msfdb start
+# Not sure how to specify the username and password form the command line
+# Need to turn antivirus software off on windows
+# see if we can exclude: 
+# /opt/metasploit-framework
+# Start the webservice
+sudo -u cyber msfdb --component webservice --address 0.0.0.0 start
+# To stop run msfdb --component webservice stop
